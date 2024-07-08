ec01f4245f3d8b19ffd5d274f63d4af023a79c34
aniyant <kumarsunny30066@gmail.com>
2024-07-08 23:24:10 +0530

    done

--- src/config/mysqldb.js
+++ src/config/mysqldb.js
@@ -2,11 +2,19 @@ const { Sequelize } = require('sequelize');
 const mysql = require('mysql2');
 const dotenv = require('dotenv');
 dotenv.config();
+console.log(process.env.SQL_DB);
 
 const sequelize = new Sequelize(process.env.SQL_DB, process.env.SQL_USER, process.env.SQL_PASSWORD, {
   host: process.env.SQL_HOST,
   port: process.env.SQL_PORT,
   dialect: 'mysql',
+
+  pool:{
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
 });
 
 module.exports = sequelize;
